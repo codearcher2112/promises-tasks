834fba07875a4e7988b2d92f21dd177f22a4eaaa
codearcher2112 <haykak5@gmail.com>
2023-11-03 12:35:35 +0400

    Resolve the third exercise of 'What will be the output of the this code?'

--- promiseWhatWillBeTheOutput.js
+++ promiseWhatWillBeTheOutput.js
@@ -41,3 +41,29 @@ Promise.resolve()
 
 // Api Error
 // 2
+
+/* 3 */
+
+console.log(1);
+
+setTimeout(() => {
+    console.log(2);
+}, 20);
+
+Promise.resolve()
+    .then(() => {
+        console.log(3);
+        return 1;
+    })
+    .catch(e => console.log(e, 4))
+    .finally(res => console.log(res, 5))
+    .then((res) => console.log(res));
+
+console.log(6);
+
+// 1
+// 6
+// 3
+// undefined 5
+// 1
+// 2 
